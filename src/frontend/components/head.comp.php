ba1c72d54d80d05fdc01046d764eaf78ae33b5a9
Kai Florian Wessela <kai@wessela.eu>
2020-05-10 18:17:44 +0200

    add components: head, header, footer

---
+++ src/frontend/components/head.comp.php
@@ -0,0 +1,3 @@
+<meta charset="utf-8">
+<base href="home.local">
+<link rel="stylesheet" type="text/css" href="/resources/css/style.css">
---
+++ src/frontend/components/header.comp.php
@@ -0,0 +1,8 @@
+<header>
+	<nav>
+		<ul>
+			<a href="/"><li>Startseite</li></a>
+			<a href="/posts"><li>Artikel</li></a>
+		</ul>
+	</nav>
+</header>

ef9f385400b040606328a549296532089ac2536d
Kai Florian Wessela <kai@wessela.eu>
2020-06-16 12:16:36 +0200

    use php echo shortcut

--- src/frontend/templates/_post.tmp.php
+++ src/frontend/templates/_post.tmp.php
@@ -2,11 +2,11 @@
 <html lang="de">
 	<head>
 		<?php include COMPONENT_PATH . 'head.comp.php'; ?>
-		<title><?php echo $post->headline; ?> – Kai Florian Wessela</title>
-		<link rel="canonical" href="https://kaiwessela.de/posts/<?php echo $post->longid; ?>">
-		<meta name="author" content="<?php echo $post->author; ?>">
-		<meta name="description" content="<?php echo $post->teaser; ?>">
-		<meta name="date" content="<?php echo to_html_time($post->timestamp); ?>">
+		<title><?= $post->headline ?> – Kai Florian Wessela</title>
+		<link rel="canonical" href="https://kaiwessela.de/posts/<?= $post->longid ?>">
+		<meta name="author" content="<?= $post->author ?>">
+		<meta name="description" content="<?= $post->teaser ?>">
+		<meta name="date" content="<?= to_html_time($post->timestamp) ?>">
 	</head>
 	<body>
 		<?php include COMPONENT_PATH . 'header.comp.php'; ?>
@@ -16,23 +16,23 @@
 <?php
 if(!empty($post->overline)){
 	?>
-					<p class="overline"><?php echo $post->overline; ?></p><!-- NOTE: is p semantically correct? -->
+					<p class="overline"><?= $post->overline ?></p><!-- NOTE: is p semantically correct? -->
 	<?php
 }
 ?>
-					<h1><span><?php echo $post->headline; ?></span></h1>
+					<h1><span><?= $post->headline ?></span></h1>
 <?php
 if(!empty($post->subline)){
 	?>
-					<p class="subline"><?php echo $post->subline; ?></p>
+					<p class="subline"><?= $post->subline ?></p>
 	<?php
 }
 ?>
 					<p class="author-and-date">
 						<!-- IDEA use address element? -->
-						Von <?php echo $post->author; ?>, <wbr>veröffentlicht am
-						<time datetime="<?php echo to_html_time($post->timestamp); ?>">
-							<?php echo to_date($post->timestamp); ?>
+						Von <?= $post->author ?>, <wbr>veröffentlicht am
+						<time datetime="<?= to_html_time($post->timestamp) ?>">
+							<?= to_date($post->timestamp) ?>
 						</time>
 					</p>
 				</header>
@@ -45,7 +45,7 @@ if(isset($post->image)){
 }
 ?>
 
-				<?php echo $parsedown->text($post->content); ?>
+				<?= $parsedown->text($post->content) ?>
 			</article>
 		</main>
 		<?php include COMPONENT_PATH . 'footer.comp.php'; ?>

5ddf09a6b9209c4a1fcebbee14c171dba2f8381f
Kai Florian Wessela <kai@wessela.eu>
2020-08-10 19:27:00 +0200

    draft new pagination

---
+++ src/frontend/web/modules/pagination/templates/default.item.php
@@ -0,0 +1,5 @@
+<a href="<?= Config::SERVER_URL ?>"
+	class="pagination-item default"
+	title="">
+	<?= $item->relative_number ?>
+</a>
---
+++ src/frontend/web/modules/pagination/templates/main.php
@@ -0,0 +1,3 @@
+<div class="pagination">
+	<?php $pagination->display_items(); ?>
+</div>

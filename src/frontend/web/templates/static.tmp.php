a66607108f78a91ef7b81a1ee663c23f9e4ba21b
Kai Florian Wessela <kai@wessela.eu>
2020-08-17 22:11:51 +0200

    bugfix git merge

--- src/frontend/web/templates/static.tmp.php
+++ src/frontend/web/templates/static.tmp.php
@@ -5,11 +5,7 @@ use \Blog\Frontend\Web\SiteConfig;
 <html lang="de">
 	<head>
 		<?php include COMPONENT_PATH . 'head.comp.php'; ?>
-<<<<<<< HEAD:src/frontend/web/templates/default.tmp.php
-		<title><?= $title ?> – <?= SiteConfig::TITLE ?></title>
-=======
 		<title><?= $StaticController->title ?> – <?= SiteConfig::TITLE ?></title>
->>>>>>> 22066990995bdc7b197e3d6a0a8cb817f13da61c:src/frontend/web/templates/static.tmp.php
 	</head>
 	<body>
 		<?php include COMPONENT_PATH . 'header.comp.php'; ?>

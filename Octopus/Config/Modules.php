56f635e7cee463edfa761dfa8ea21e237e70d993
Kai Florian Wessela <kai@wessela.eu>
2022-03-30 16:27:26 +0200

    new module config

--- Octopus/Config/Modules.php
+++ Octopus/Config/Modules.php
@@ -1,17 +1,23 @@
 <?php
 return [
-	'@include' => [
-		\Octopus\Modules\Events\Event::class 		=> 'Modules/Events/EventConfig.php',
-		\Octopus\Modules\Media\Application::class 	=> 'Modules/Media/ApplicationConfig.php',
-		\Octopus\Modules\Media\Audio::class 		=> 'Modules/Media/AudioConfig.php',
-		\Octopus\Modules\Media\Image::class 		=> 'Modules/Media/ImageConfig.php',
-		\Octopus\Modules\Media\Video::class 		=> 'Modules/Media/VideoConfig.php',
-		\Octopus\Modules\Motions\Motion::class 		=> 'Modules/Motions/MotionConfig.php',
-		\Octopus\Modules\Pages\Page::class 			=> 'Modules/Pages/PageConfig.php',
-		\Octopus\Modules\Persons\Person::class 		=> 'Modules/Persons/PersonConfig.php',
-		\Octopus\Modules\Persons\Group::class 		=> 'Modules/Persons/GroupConfig.php',
-		\Octopus\Modules\Posts\Post::class 			=> 'Modules/Posts/PostConfig.php',
-		\Octopus\Modules\Posts\Column::class 		=> 'Modules/Posts/ColumnConfig.php',
+	'controllers' => [
+		'BasicEntityController' => '\Octopus\Modules\BasicEntityController\BasicEntityController'
+	],
+	'entities' => [
+		'Event' 		=> '\Octopus\Modules\Events\Event',
+		'Application' 	=> '\Octopus\Modules\Media\Application',
+		'Audio' 		=> '\Octopus\Modules\Media\Audio',
+		'Image' 		=> '\Octopus\Modules\Media\Image',
+		'Video' 		=> '\Octopus\Modules\Media\Video',
+		'Motion' 		=> '\Octopus\Modules\Motions\Motion',
+		'Page' 			=> '\Octopus\Modules\Pages\Page',
+		'Person' 		=> '\Octopus\Modules\Persons\Person',
+		'Group' 		=> '\Octopus\Modules\Persons\Groups\Group',
+		'Post' 			=> '\Octopus\Modules\Posts\Post',
+		'Column' 		=> '\Octopus\Modules\Posts\Columns\Column'
+	],
+	'default_entity_controllers' => [
+		'@all' => 'BasicEntityController'
 	]
 ];
 ?>
